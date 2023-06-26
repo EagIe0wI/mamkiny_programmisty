315ca304b340a04daee48781b6d64374234a7ea8
root <root@LAPTOP-7J85VK73>
2023-06-26 13:42:57 +0300

    make text output

--- src/script.js
+++ src/script.js
@@ -9,8 +9,29 @@ const gameBoard = document.querySelector('.game-board');
     });
 };
 
+const changeText = () => {
+    const gameBoard = document.querySelector('.game-board');
+
+    const sentences = [
+        '*Вы начинаете бродить по лесу*',
+        '*Неожиданно появляется огонёк*',
+        'Огонёк:\nЗдравствуй, путник! Неужели потерялся?',
+        'Что ты такое?',
+        'Огонёк:\nКак невежливо не отвечать на приветствие! Ну ладно… Плутающие души зачастую сильно напуганы, им можно простить.',
+        '',
+        ''
+    ]
+    let sentence = 0;
+    gameBoard.addEventListener('click', () => {
+       
+       document.querySelector('.text').textContent = sentences[sentence];
+       sentence += 1;
+       });
+    }
+
 const start = () => {
     buttonForStart();
+    changeText();
 }
 
 start();
