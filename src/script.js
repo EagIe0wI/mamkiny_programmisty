07e6a419893348cbcdea228659a8667404beff54
root <root@LAPTOP-7J85VK73>
2023-06-28 18:10:33 +0300

    fix text code

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 
+
 const buttonForStart = () => {
 const startButton = document.querySelector('.start-button');
 const gameBoard = document.querySelector('.game-board');
@@ -12,39 +13,27 @@ const gameBoard = document.querySelector('.game-board');
 const changeText = () => {
     const gameBoard = document.querySelector('.game-board');
     const firstFork = document.querySelector('.form-YesOrNo');
-
-    const sentences = [
-        '*Вы начинаете бродить по лесу*',
-        '*Неожиданно появляется огонёк*',
-        'Огонёк:\nЗдравствуй, путник! Неужели потерялся?',
-        'Вы:\nЧто ты такое?',
-        'Огонёк:\nКак невежливо не отвечать на приветствие! Ну ладно… Плутающие души зачастую сильно напуганы, им можно простить.',
-        'Вы:\nПлутающие души?...',
-        'Огонёк:\nАх! Так о чём же я? Не нужно знать кто я и откуда. Просто доверься мне, я помогу тебе выбраться',
-        'Огонёк:\nТолько если ты захочешь.',
-        'Вы:\n*мысли* Мне кажется или это дьявольское отродье только что ухмыльнулось?',
-        'Огонёк:\nТак что? Тебе нужна помощь?'
-    ]
+    const stage = 'start';
+    
     let sentence = 0;
     gameBoard.addEventListener('click', () => {
        
-       document.querySelector('.text').textContent = sentences[sentence];
+       document.querySelector('.text').textContent = strings[stage][sentence];
        sentence += 1;
-       if (sentence === sentences.length - 1) {
-        document.querySelector('.text').textContent = sentences[sentences.length - 1];
+       if (sentence === 5) {
+        document.querySelector('.text').textContent = strings[strings.length - 1];
         firstFork.style.display = 'block';
     }
        });
-
     }
 
 
 
     const quizCheck = () => {
-        var correctAnswer = 3;
+        let correctAnswer = 3;
  
         document.getElementById("check").onclick = function() {
-        var answer = document.getElementById("form").querySelectorAll('[name="answer-1"]:checked');
+        let answer = document.getElementById("form").querySelectorAll('[name="answer-1"]:checked');
         if (!answer.length) {
          answer = 0;
         } else {
